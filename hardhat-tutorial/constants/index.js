a11f0bcbb23c7d7f19f6f7c20c03e7760c0531eb
Alan Pelayo Zepeda <alanpelayozepeda@Alans-MacBook-Air.local>
2022-08-09 16:33:48 -0500

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xEa28748AD952E1430a7db83F5AbC047Db507e1D0";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
